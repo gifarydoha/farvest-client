cdbed92294c61aee5fc0ff645094e7fa75e7fbd3
Doha0 <abu.jardoha@gmail.com>
2024-06-06 10:07:18 +0600

    footer added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Banner from "@/components/Home/Banner/Banner";
 import Parallax from "@/components/Home/Parallax/Parallax";
 import Resources from "@/components/Home/Resources/Resources";
 import Works from "@/components/Home/Works/Works";
+import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 import React from "react";
 
@@ -13,6 +14,7 @@ const page = () => {
       <Works />
       <Parallax />
       <Resources />
+      <Footer />
     </section>
   );
 };
