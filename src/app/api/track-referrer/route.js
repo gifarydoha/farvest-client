2b1456a42f783de92f67b0548fc71575605e6bd8
gifarydoha <abu.jardoha@gmail.com>
2025-05-01 09:46:34 +0600

    console removed

--- src/app/api/track-referrer/route.js
+++ src/app/api/track-referrer/route.js
@@ -5,7 +5,7 @@ export async function POST(request) {
     const body = await request.json();
     const { referrer, pathname, timestamp } = body;
 
-    console.log("[API REQUEST]", body);
+    // console.log("[API REQUEST]", body);
 
     const client = await clientPromise;
     const db = client.db("farvestDB");
@@ -17,18 +17,18 @@ export async function POST(request) {
       timestamp: timestamp ? new Date(timestamp) : new Date(),
     };
 
-    console.log("[FINAL DOC TO INSERT]", document);
+    // console.log("[FINAL DOC TO INSERT]", document);
 
     try {
       const result = await collection.insertOne(document);
-      console.log("[INSERTED]", result);
+      // console.log("[INSERTED]", result);
     } catch (insertErr) {
-      console.error("Insert Error:", insertErr);
+      // console.error("Insert Error:", insertErr);
     }
 
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (error) {
-    console.error("Referrer Tracking Error:", error);
+    // console.error("Referrer Tracking Error:", error);
     return new Response(
       JSON.stringify({ success: false, error: error.message }),
       { status: 500 }
